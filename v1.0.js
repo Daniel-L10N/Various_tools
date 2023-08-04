9ee72c26958e7c1b028c84c3c5deb9831e8386d8
Daniel Alejandro <136534872+Daniel-L10N@users.noreply.github.com>
2023-08-04 02:31:59 -0400

    variable renaming

--- v1.0.js
+++ v1.0.js
@@ -1,11 +1,10 @@
-// 
 //##########################################################################
 //--------------------Hourly to Salary--------------------//
 //##########################################################################
-var Hourly_to_Salary= function(pay, hours, weeks) {
+var Hourly_to_Salary= function(p, h, w) {
     //variable names:
-    var per_Week_Worked = (Number(pay * hours).toFixed(2))
-    var annual_Salary = (Number(per_Week_Worked * weeks).toFixed(2))
+    var per_Week_Worked = (Number(p * h).toFixed(2))
+    var annual_Salary = (Number(per_Week_Worked * w).toFixed(2))
     var monthly_Pay  = (Number(annual_Salary / 12).toFixed(2))
     var median_salary = (Number((annual_Salary / 57200) *100).toFixed(1)) 
     return  [per_Week_Worked, annual_Salary, monthly_Pay, median_salary]
@@ -23,12 +22,12 @@ console.log(`\n\n`)
 //--------------------Funcion Salary to Hourly Anual--------------------//
 //##########################################################################
 
-var Salary_to_Hourly_Anual = function(pay, hours, weeks) {
+var Salary_to_Hourly_Anual = function(p, h, w) {
     //variable names:
-    var hourly_pay = (Number((pay / weeks) /hours).toFixed(2))
-    var per_week = (Number(pay / weeks).toFixed(2))
-    var monthly_pay = (Number(pay / 12).toFixed(2))
-    var median_salary = (Number((pay / 57200) * 100).toFixed(1))
+    var hourly_pay = (Number((p / w) /h).toFixed(2))
+    var per_week = (Number(p / w).toFixed(2))
+    var monthly_pay = (Number(p / 12).toFixed(2))
+    var median_salary = (Number((p / 57200) * 100).toFixed(1))
     return [hourly_pay, per_week, monthly_pay,median_salary]
      
 }
@@ -40,12 +39,12 @@ console.log(`\n\n`)
 //--------------------Funcion Salary to Hourly Monthly--------------------//
 //##########################################################################
 
-var Salary_to_Hourly_Monthly = function(pay, hours, weeks) {
+var Salary_to_Hourly_Monthly = function(p, h, w) {
     //variable names:
-    var monthly_pay = (Number(pay).toFixed(2))
-    var per_week = (Number((pay * 12) / weeks).toFixed(2))
-    var hourly_pay = (Number(per_week/ hours).toFixed(2))
-    var median_salary = (Number((pay / (57200/ 12)) * 100).toFixed(1))
+    var monthly_pay = (Number(p).toFixed(2))
+    var per_week = (Number((p * 12) / w).toFixed(2))
+    var hourly_pay = (Number(per_week/ h).toFixed(2))
+    var median_salary = (Number((p / (57200/ 12)) * 100).toFixed(1))
     return [hourly_pay, per_week, monthly_pay,median_salary]
      
 }
@@ -59,10 +58,10 @@ console.log(`\n\n`)
 //--------------------Funcion Salary to Hourly Biweekly--------------------//
 //##########################################################################
 
-var Salary_to_Hourly_Biweekly = function(pay, hours, weeks) {
+var Salary_to_Hourly_Biweekly = function(p, h, w) {
     //variable names:
-    var monthly_pay = (Number(pay * 2.166666667).toFixed(2))
-    var per_week = (Number(monthly_pay *12 / week).toFixed(2))
+    var monthly_pay = (Number(p * 2.166666667).toFixed(2))
+    var per_week = (Number(monthly_pay *12 / w).toFixed(2))
     var hourly_pay = (Number(per_week/ h).toFixed(2))
     var median_salary = (Number((monthly_pay  / (57200/ 12)) * 100).toFixed(1))
     return [hourly_pay, per_week, monthly_pay,median_salary]
@@ -78,11 +77,11 @@ console.log(`\n\n`)
 //--------------------Funcion Salary to Hourly Weekly--------------------//
 //##########################################################################
 
-var Salary_to_Hourly_Weekly = function(pay, hours, week) {
+var Salary_to_Hourly_Weekly = function(p, h, w) {
     //variable names:
-    var monthly_pay = (Number(pay * week /12).toFixed(2))
-    var per_week = (Number(pay).toFixed(2))
-    var hourly_pay = (Number(pay / hours).toFixed(2))
+    var monthly_pay = (Number(p * w /12).toFixed(2))
+    var per_week = (Number(p).toFixed(2))
+    var hourly_pay = (Number(p / h).toFixed(2))
     var median_salary = (Number((monthly_pay  / (57200 / 12)) * 100).toFixed(1))
     return [hourly_pay, per_week, monthly_pay,median_salary]
      
@@ -97,11 +96,11 @@ console.log(`\n\n`)
 //--------------------Funcion Salary to Hourly Daily--------------------//
 //##########################################################################
 
-var Salary_to_Hourly_Daily = function(pay, hours, weeks) {
+var Salary_to_Hourly_Daily = function(p, h, w) {
     //variable names:
-    var per_week = (Number(pay/ 8 * 40).toFixed(2))
-    var monthly_pay = (Number(per_week * weeks /12).toFixed(2))
-    var hourly_pay = (Number(pay/ (hours / 5) ).toFixed(2))
+    var per_week = (Number(p/ 8 * 40).toFixed(2))
+    var monthly_pay = (Number(per_week * w /12).toFixed(2))
+    var hourly_pay = (Number(p/ (h / 5) ).toFixed(2))
     var median_salary = (Number((monthly_pay  / (57200 / 12)) * 100).toFixed(1))
     return [hourly_pay, per_week, monthly_pay,median_salary]
      
@@ -114,14 +113,14 @@ console.log(`\n\n`)
 //--------------------Funcion Time and a Half--------------------//
 //##########################################################################
 
-var Time_and_a_Half_Hourly = function(hourly, worked_at_time_and_a_half, hours_standard){
-    var Standard_hourly_rate  = hourly 
-    var Time_and_a_half_rate  = hourly * 1.50
-    var Time_and_a_half_pay   = hourly * (1.50*worked_at_time_and_a_half)
-    var Standard_pay = hours_standard * hourly
+var Time_and_a_Half_Hourly = function(a,b=0,c){
+    var Standard_hourly_rate  = a 
+    var Time_and_a_half_rate  = a * 1.50
+    var Time_and_a_half_pay   = a * (1.50*b)
+    var Standard_pay = c * a
     var Total_money_earned = Time_and_a_half_pay + Standard_pay
     return [Time_and_a_half_rate,Standard_hourly_rate , Time_and_a_half_pay,Standard_pay, Total_money_earned ]
 }
-result6= Time_and_a_Half_Hourly(15,0,1 )
+result6= Time_and_a_Half_Hourly(65,4,4 )
 console.log(`Calculation Results: \n\t Time and a half rate: ${result6[0]} \n\t Standard hourly rate: ${result6[1]} \n\t Time and a half pay: ${result6[2]} \n\t Standard pay: ${result6[3]} \n\t Total money earned: ${result6[4]}`)
 
